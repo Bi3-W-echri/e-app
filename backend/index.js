fd9e2639007d92423b53043f8a19eebe9c2117b8
cherif1999 <saifoujab@gmail.com>
2024-02-13 18:05:13 +0100

    server

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,15 @@
-//hi
-//hello
+const express = require('express');
+const PORT = 3000;
+const app = express();
+const cors = require("cors")
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/api', (req, res) => {
+  res.json('Hello from the server!');
+})
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
